63893fc92d904f27d9887441333dc4b0aeb9ac51
Joshua Reade Harris <accounts@jrh.place>
2022-08-03 16:04:42 +0100

    Add image.

--- ui/src/modules/HomeScreenInstructions/AppleInstructions/AppleInstructions.svelte
+++ ui/src/modules/HomeScreenInstructions/AppleInstructions/AppleInstructions.svelte
@@ -1,8 +1,17 @@
 <script>
   export let skip
+
+  import appleStepTwo from './assets/apple-step-2.png'
 </script>
 
 <section id='apple-instructions'>
   <h1>Apple</h1>
+  <img src={ appleStepTwo } alt='Instructions Step 2' />
   <button on:click={ skip }>Skip</button>
 </section>
+
+<style lang='stylus'>
+  img
+    margin 0 auto
+    width 200px
+</style>
