2179a9a456e5ce60387e3efed36c6fb9f0c5890e
Elsa Brown <elsambrown@gmail.com>
2022-08-04 13:01:00 -0400

    [UI] Build and style home screen instructions for iOS and Android.

--- ui/src/modules/HomeScreenInstructions/AppleInstructions/AppleInstructions.svelte
+++ ui/src/modules/HomeScreenInstructions/AppleInstructions/AppleInstructions.svelte
@@ -1,17 +1,56 @@
 <script>
   export let skip
 
+  import appleStepOne from './assets/apple-step-1.png'
   import appleStepTwo from './assets/apple-step-2.png'
+  import appleStepThree from './assets/apple-step-3.png'
+  import appleStepFour from './assets/apple-step-4.png'
+  
 </script>
 
 <section id='apple-instructions'>
-  <h1>Apple</h1>
-  <img src={ appleStepTwo } alt='Instructions Step 2' />
-  <button on:click={ skip }>Skip</button>
+  <p class="intro">CGPay is easy to use when you add it to your device home screen.</p>
+  <button on:click={ skip }>Skip for now</button>
+  <h2>Steps to save:</h2>
+  <ul>
+    <li>
+      <p>1. Tap the Share icon from iOS banner at the bottom of your screen.</p>
+      <img src={ appleStepOne } alt='Instructions Step 1' />
+    </li>
+    <li>
+      <p>2. Scroll down and select “Add To Home Screen” from menu items.</p>
+      <img src={ appleStepTwo } alt='Instructions Step 2' />
+    </li>
+    <li>
+      <p>3. Tap “Add” to save CGPay to your home screen.</p>
+      <img src={ appleStepThree } alt='Instructions Step 3' />
+    </li>
+    <li>
+      <p>4. The CGPay icon will be saved to your home screen. Tap it to open CGPay and continue.</p>
+      <img src={ appleStepFour } alt='Instructions Step 4' class="icon" />
+    </li>
+  </ul>
+  
 </section>
 
 <style lang='stylus'>
+  button
+    text('sm')
+    link()
+    margin 0 0 $s3
+  h2
+    font-weight: bold
+    margin 0 0 $s1
   img
     margin 0 auto
-    width 200px
+    padding 0 $s1
+  li
+    margin 0 0 $s3
+  p
+    margin 0 0 $s2
+  .icon
+    max-width: 100px
+  .intro
+    margin 0 0 $s1
+
 </style>
--- ui/src/modules/HomeScreenInstructions/HomeScreenInstructions.svelte
+++ ui/src/modules/HomeScreenInstructions/HomeScreenInstructions.svelte
@@ -5,6 +5,8 @@
   import AppleInstructions from './AppleInstructions/AppleInstructions.svelte'
   import preferencesStore from '../Preferences/preferences.store.js'
 
+  import cgLogo from './assets/cg-logo-300.png'
+
   // --------------------------------------------
 
   const { userAgent } = window.navigator
@@ -24,13 +26,34 @@
 </svelte:head>
 
 <section id='home-screen-instructions'>
-  <h1>Add to Home Screen</h1>
+  <div class="wrapper">
+    <img src= { cgLogo } alt="Common Good logo" />
+    <h1>Add to Home Screen</h1>
+    
+    { #if onAppleDevice }
+      <AppleInstructions { skip } />
+    { /if }
 
-  { #if onAppleDevice }
-    <AppleInstructions { skip } />
-  { /if }
-
-  { #if onAndroidDevice }
-    <AndroidInstructions { skip } />
-  { /if }
+    { #if onAndroidDevice }
+      <AndroidInstructions { skip } />
+    { /if }
+  </div>
 </section>
+
+<style lang='stylus'>
+  h1
+    text(lg)
+    font-weight bold
+    margin 0 0 $s2
+  
+  img
+    margin 0 0 $s2
+    width 6rem
+
+  .wrapper
+    flexCenter(column)
+    background $white
+    border solid 1px $black
+    margin $s2
+    padding: $s2 $s2 $s3
+</style>
