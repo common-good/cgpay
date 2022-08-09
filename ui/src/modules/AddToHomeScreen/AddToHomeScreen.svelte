f541ae7aea16cbe1dd327422139321af323e9243
Joshua Reade Harris <accounts@jrh.place>
2022-08-09 22:33:25 +0100

    [UI] Style Add to Home Screen screen.

--- ui/src/modules/AddToHomeScreen/AddToHomeScreen.svelte
+++ ui/src/modules/AddToHomeScreen/AddToHomeScreen.svelte
@@ -6,6 +6,14 @@
   import AndroidInstructions from './AndroidInstructions/AndroidInstructions.svelte'
   import AppleInstructions from './AppleInstructions/AppleInstructions.svelte'
 
+  import cgLogo from './assets/cg-logo-300.png?webp'
+
+  // --------------------------------------------
+
+  const { userAgent } = window.navigator
+  const onAndroidDevice = /Android/i.test(userAgent)
+  const onAppleDevice = /iPhone|iPod|iPad/i.test(userAgent)
+
   // --------------------------------------------
 
   function skip() {
@@ -19,13 +27,34 @@
 </svelte:head>
 
 <section id='add-to-home-screen'>
-  <h1>Add to Home Screen</h1>
+  <div class='wrapper'>
+    <img src= { cgLogo } alt="Common Good logo" />
+    <h1>Add to Home Screen</h1>
 
-  { #if $store.deviceType === 'Apple' }
-    <AppleInstructions { skip } />
-  { /if }
+    { #if $store.deviceType === 'Apple' }
+      <AppleInstructions { skip } />
+    { /if }
 
-  { #if $store.deviceType === 'Android' }
-    <AndroidInstructions { skip } />
-  { /if }
+    { #if $store.deviceType === 'Android' }
+      <AndroidInstructions { skip } />
+    { /if }
+  </div>
 </section>
+
+<style lang='stylus'>
+  h1
+    text lg
+    font-weight 600
+    margin 0 0 $s2
+
+  img
+    margin 0 0 $s2
+    width 6rem
+
+  .wrapper
+    flexCenter column
+    background $white
+    border solid 1px $black
+    margin $s2
+    padding $s2 $s2 $s3
+</style>
--- ui/src/modules/AddToHomeScreen/AndroidInstructions/AndroidInstructions.svelte
+++ ui/src/modules/AddToHomeScreen/AndroidInstructions/AndroidInstructions.svelte
@@ -3,6 +3,16 @@
 </script>
 
 <section id='android-instructions'>
-  <h1>Android</h1>
-  <button on:click={ skip }>Skip</button>
+  <p>CGPay is easy to use when you add it to your device home screen.</p>
+  <p>Click the banner below to add CGPay to your home screen.</p>
+  <button on:click={ skip }>Skip for now</button>
 </section>
+
+<style lang='stylus'>
+  button
+    link null
+    text sm
+
+  p
+    margin 0 0 $s2
+</style>
