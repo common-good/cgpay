5dff78c26acae8538beee033b8f47ec7ceab39fe
William Spademan <wspademan@CommonGood.earth>
2023-01-17 12:32:06 -0500

    Auto stash before merge of "offline" and "origin/develop"

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,10 +1,10 @@
-import to from '@adaptably/to';
-import { VitePWA } from 'vite-plugin-pwa';
-import { imagetools } from 'vite-imagetools';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import to from '@adaptably/to'
+import { VitePWA } from 'vite-plugin-pwa'
+import { imagetools } from 'vite-imagetools'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-import adapt from '@adaptably/adapt';
-import pwaConfig from './vite.pwa.config.js';
+import adapt from '@adaptably/adapt'
+import pwaConfig from './vite.pwa.config.js'
 
 // --------------------------------------------
 
@@ -17,7 +17,7 @@ export default {
 
   resolve: {
     alias: {
-      '#store.js': to('./src/app.store.js', { from: import.meta.url }),
+      '#store.js': to('./src/store.js', { from: import.meta.url }),
       '#utils.js': to('./src/utils.js', { from: import.meta.url }),
       '#db.js': to('./src/db.js', { from: import.meta.url }),
       '#modules': to('./src/modules', { from: import.meta.url }),
