241cdb6441fe62819e6a05ebe0402e761e3760ef
Elsa Brown <elsambrown@gmail.com>
2023-03-13 15:39:52 -0400

    Updates Add to Home Screen test

--- tests/features/add-to-home-screen.feature
+++ tests/features/add-to-home-screen.feature
@@ -1,20 +1,24 @@
 Feature: Add To Home Screen
 
 Background:
-  * mobile device
-   * Apple i0S
-   * Android
+  * context
+  * Apple i0S
+  * Android
 
-Rule: Mobile device users are prompted to add the app to their home screen
+Rule: Users are prompted to add the app to their home screen with device-specific instructions
 
-Scenario: I visit the CGPay app online
-  When I visit the CGPay app online
-  Then ? I am prompted to install the bookmark to my home screen
+Scenario: I visit CGPay online
+  When I have not been prompted to add the app to my home screen
+  Then ? I am prompted to install the app to my home screen
+
+Scenario: I visit CGPay online
+  When I have previously been prompted to add the app to my home screen
+  Then ? I am directed to the sign-in page
 
 Scenario: I am on an iOS device
   When I visit the CGPay app online
-  Then ? I am prompted to install the bookmark to my home screen
+  Then ? I am prompted to install the app to my home screen
 
 Scenario: I am on an Android device
   When I visit the CGPay app online
-  Then ? I am prompted to install the bookmark to my home screen
+  Then ? I am prompted to install the app to my home screen
--- tests/steps/steps.js
+++ tests/steps/steps.js
@@ -1,6 +1,8 @@
 const { Given, When, Then } = require("@cucumber/cucumber")
 const { expect } = require('@playwright/test')
 
+const site = 'app.commongood.earth'
+
 When('context', async function () {
   const pw = await this.pw()
   this.browser = pw.browser
@@ -8,11 +10,24 @@ When('context', async function () {
   this.page = pw.page
 })
 
-When('I visit {string}', async function (site) {
+Given('I visit CGPay online', async function (site) {
   await this.page.goto('https://' + site)
 })
 
-Then('? the page title is {string}', async function (wantTitle) {
+When('I have not been prompted to add the app to my home screen', async() => {
+  // canAddToHomeScreen is true
+})
+
+Then('? I am prompted to install the app to my home screen', async function () {
   const gotTitle = await this.page.title()
-  expect(gotTitle).toEqual(wantTitle);
+  expect(gotTitle).toEqual('CGPay - Add to Home Screen');
+})
+
+When('I have previously been prompted to add the app to my home screen', async() => {
+  // canAddToHomeScreen is false
+})
+
+Then('? I am directed to the sign-in page', async function () {
+  const gotTitle = await this.page.title()
+  expect(gotTitle).toEqual('CGPay - Sign In');
 })
