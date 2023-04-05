4e181ac858d4dc742bf6b28019e6ee7910c1b5d3
Elsa Brown <elsambrown@gmail.com>
2023-04-05 16:02:18 -0400

    Cleans up Survey CTA copy

--- src/modules/Home.svelte
+++ src/modules/Home.svelte
@@ -74,7 +74,7 @@
       </div>
     { /if }
     { #if c.isReleaseA }
-      <a class="survey" href="https://forms.gle/M8Hv1W2oSgw2yQzS7" target="_blank">Take Our User Experience Survey</a>
+      <a class="survey" href="https://forms.gle/M8Hv1W2oSgw2yQzS7" target="_blank">Take Our User Survey</a>
     { :else }
       <a class="scan-customer" href='/scan'>Scan QR Code to Charge</a>
     { /if }
