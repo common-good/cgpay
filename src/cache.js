9bb5a03c24fdb1329851cb2ab64f8cadf2db6595
William Spademan <wspademan@CommonGood.earth>
2023-04-06 10:25:35 -0400

    added comments to cache.js

--- src/cache.js
+++ src/cache.js
@@ -1,24 +1,24 @@
 // initial values for store
 
 const cache = {
-  sawAdd: false,
-  qr: null,
+  sawAdd: false, // user pressed Continue on the Add-to-home-screen page
+  qr: null, // scanned qr URL
   lastOp: null, // timestamp of last operation to timeout
   msg: null, // not yet used
-  erMsg: null,
+  erMsg: null, // error message to display on Home page
   cameraCount: 0, // set this when scanning for the first time
   frontCamera: null, // set this in Root.svelte (can't be defaulted from tests)
-  online: null,
-  useWifi: true,
-  selfServe: false,
+  online: null, // true if app is online
+  useWifi: true, // false if developer or test framework has disabled wifi
+  selfServe: false, // self-serve mode
 
-  choices: null,
-  txs: [],
-  comments: [],
+  choices: null, // accounts that user has permission to use in the app
+  txs: [], // transactions waiting to be uploaded
+  comments: [], // comments waiting to be uploaded
 
-  corrupt: null,
-  accts: {},
-  myAccount: null,
+  corrupt: null, // timestamp that cached data got corrupted
+  accts: {}, // keyed list of accounts that user has transacted with (or tried to)
+  myAccount: null, // information about user's account, signed in
 
   fromTester: {}, // values to update at the request of the test framework (see hooks.js and t.putv())
 }
