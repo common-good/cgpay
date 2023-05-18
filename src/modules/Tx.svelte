061786a304bb62e046fdc76e46f6b0f15744174f
Elsa Brown <elsambrown@gmail.com>
2023-05-18 11:02:37 -0400

    Adds new tx screen structure

--- src/modules/Tx.svelte
+++ src/modules/Tx.svelte
@@ -1,6 +1,49 @@
 <script>
   import st from'#store.js'
-  console.log($st.intent)
+  
+
+  const intent = $st.intent
+  const qr = intent === 'pay' ? $st.myAccount?.qr : null
+  const qrAction = `Show this code to ${intent === 'pay' ? intent : 'be paid'}`
 </script>
 
-<div>PAY OR CHARGE</div>
+<section class="page" id="tx">
+  <div class="top">
+    <h1>{intent}</h1>
+    <p>{qrAction}</p>
+    <img src="{qr}" data-testid="qr" alt={qrAction} />
+  </div>
+  <div class="bottom">
+    <button>Scan to {intent}</button>
+  </div>
+</section>
+
+<style lang='stylus'>
+  button
+    cgButton()
+    text-transform capitalize
+
+  h1 
+    text-transform capitalize
+
+  img
+    min-height 300px
+
+  section
+    height 100%
+    display flex
+    flex-direction column
+    align-items center
+    justify-content space-between
+
+  .top
+    width 100%
+    height 100%
+    display flex
+    flex-direction column
+    align-items center
+
+  .bottom
+    width 100%
+    text-align center
+</style>
