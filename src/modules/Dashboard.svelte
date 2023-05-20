cd4d145c0d6443131bf6604ccb24d50882879f7f
William Spademan <wspademan@CommonGood.earth>
2023-05-20 18:28:23 -0400

    add rudimentary data to Dashboard

--- src/modules/Dashboard.svelte
+++ src/modules/Dashboard.svelte
@@ -4,4 +4,9 @@
 </script>
 <section id="dashboard">
   <p>Account: {$st.myAccount?.accountId}</p>
+  <p>Balance: ${balance}</p>
+  <h2>Recent Transactions: {#if !recents.length}(none){/if}</h2>
+  {#each recents as tx}
+    <p>{tx.created}: {tx.who}${tx.amount} (#{tx.id})</p>
+  {/each}
 </section>
