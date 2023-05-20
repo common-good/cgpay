51821980fca115ac5f45ae2079dea95868f78709
William Spademan <wspademan@CommonGood.earth>
2023-05-20 18:05:38 -0400

    self-serve mode is a permanent option

--- src/modules/LinkAccount.svelte
+++ src/modules/LinkAccount.svelte
@@ -11,14 +11,13 @@
   let acctOpts = []
   let size = 4 // number of choices to show without scrolling (fails on Android)
   let lock = true
-  let selfServe = false // used only if c.showSelfServe and not c.showScanToPay
+  let selfServe = false // used only if not c.showScanToPay
   let myAccount
   let ready = false
   let acctIndex = null
   let payOk = c.showScanToPay ? 'scan' : 'never'
   const choices = $st.choices
   const payOkOptions = { always:'always', scan:'only if a manager scans in', never:'never', self:'self-serve mode' }
-  if (!c.showSelfServe) delete payOkOptions.self
   
   function er(msg) { u.alert(msg) } 
 
@@ -27,7 +26,7 @@
     st.setMyAccount(myAccount)
     if (lock) st.setAcctChoices(null)
     st.setPayOk(myAccount.isCo ? payOk : null)
-    if (selfServe) st.setPayOk('self') // only if c.showSelfServe and not c.showScanToPay
+    if (selfServe) st.setPayOk('self') // only if not c.showScanToPay
     u.goHome(`This device is now linked to your Common Good account: ${myAccount?.name}.`)
   }
 
@@ -65,7 +64,7 @@
             <label><input type="checkbox" data-testid="lock-account" name="lock-account" 
               bind:checked={lock} class={ lock ? 'checked' : '' }/> Require sign-in to change account</label>
           {/if}
-          {#if c.showSelfServe && !c.showScanToPay && acctIndex > 0}
+          {#if !c.showScanToPay && acctIndex > 0}
             <label><input type="checkbox" data-testid="self-serve" name="self-serve" 
               bind:checked={selfServe} class={ selfServe ? 'checked' : '' }/> Self-serve mode</label>
           {/if}
