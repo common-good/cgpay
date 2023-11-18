f7d8efecd7aa814111322abfa00d31ed5fd9587e
William Spademan <wspademan@CommonGood.earth>
2023-11-18 13:26:22 -0500

    shorten offline message, so it fits

--- src/modules/NetworkStatus.svelte
+++ src/modules/NetworkStatus.svelte
@@ -3,12 +3,12 @@
   import WifiOff from "svelte-material-icons/WifiOff.svelte"
 
   let txsWaiting
-$: txsWaiting = $st.txs.length ? (' (' + $st.txs.length + ' transactions waiting to upload)') : ''
+$: txsWaiting = $st.txs.length ? (' (' + $st.txs.length + ' transactions to upload)') : ''
 </script>
 
 { #if !$st.online }
   <section id='network-status'>
-    <span><WifiOff size={"1rem"} /></span><p data-testid="network-offline">Network is offline.{txsWaiting}</p>
+    <span><WifiOff size={"1rem"} /></span><p data-testid="network-offline">Network OFFLINE{txsWaiting}</p>
   </section>
 { /if }
 
