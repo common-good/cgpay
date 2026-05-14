60296a8bdd23671de61fabb1f3a4471660b15c21
Chris Schwab <cschwab@commongood.earth>
2026-05-14 10:21:26 -0400

    test: remove stale SelectAccount spec (component was renamed to SelectX)

--- tests/unit-tests/SelectAccount.spec.js
+++
@@ -1,9 +0,0 @@
-import SelectAccount from '#modules/SelectAccount.svelte'
-
-// --------------------------------------------
-
-describe('SelectAccount', () => {
-  it('renders', () => {
-    render(SelectAccount, {accountOptions: [], size: null})
-  })
-})
